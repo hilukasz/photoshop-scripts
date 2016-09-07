4a46af9831714f1de8a0e0272560dc0ac70b73ed
Łukasz Wieczorek <wutronic@Lukaszs-MacBook-Pro.local>
2016-09-07 15:57:37 -0700

    new library upload

---
+++ [wu]renameLayer.jsx
@@ -0,0 +1,9 @@
+﻿// written by Lukasz Wieczorek at http://lukasz.io
+// Please do not remove this and give credit if you repost. Thanks!
+var docRef  = app.activeDocument;
+
+var currentName = docRef.activeLayer.name;
+var myName=prompt("new name of layer",currentName);
+var layerLength = app.activeDocument.layers.length;
+
+docRef.activeLayer.name = myName;
---
+++ cleanPNGextention.jsx
@@ -0,0 +1,17 @@
+﻿// written by Lukasz Wieczorek at http://lukasz.io
+// Please do not remove this and give credit if you repost. Thanks!
+
+// work in progress do not use just yet.
+
+var text = "Foundation.png32 asdfasdf";
+text = text.replace(/\..*$/,"");
+
+var layers = [];
+var doc = app.activeDocument;
+
+for(i = 0; i < doc.layers.length; i++){
+    var layerName = doc.layers[i].name;
+    var newLayerName = layerName.replace(/\..*$/,"");
+    doc.layers[i].name = newLayerName;
+}
+
